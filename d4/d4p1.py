bd3e8c599c082b95d7f52e7062926a3449589e89
NichBon <bonettn@gmail.com>
2025-12-07 22:57:11 +1100

    d4p1 solution

---
+++ d4/d4p1.py
@@ -0,0 +1,41 @@
+input = open("input.txt", "r")
+
+def accessiblePaperCheck(row, column, rowCount, columnCount, paperArray):
+    if paperArray[row][column] != "@": return False
+    adjacent = -1
+    #print("column boundaries: ", column - 1 if column -1 > 0 else 0 , column + 1 if column + 1 < columnCount else column)
+    for columnToCheck in range(column - 1 if column -1 > 0 else 0 , column + 2 if column + 1 < columnCount else columnCount):
+        for rowToCheck in range(row - 1 if row -1 > 0 else 0, row + 2 if row + 1 < rowCount else rowCount):
+            #print(row)
+            try:
+                #print(columnToCheck, rowToCheck, paperArray[rowToCheck][columnToCheck])
+                if paperArray[rowToCheck][columnToCheck] != ".":
+                    adjacent += 1
+            except: print("FROM: ", row, column, "Out of bounds: ", rowToCheck, columnToCheck)
+    #print(adjacent)
+    if adjacent < 4:
+        return True
+    return False
+    
+
+paperArray = []
+for line in input:
+    paperArray.append(line.strip('\n'))
+columnCount = len(paperArray[0])
+rowCount = len(paperArray)
+
+accessibleRolls = 0
+
+for rowIndex, row in enumerate(paperArray):
+    currentRow = list(paperArray[rowIndex])
+    for columnIndex, cell in enumerate(row):
+        if (accessiblePaperCheck(rowIndex, columnIndex, rowCount, columnCount, paperArray)):
+                accessibleRolls += 1
+                currentRow[columnIndex] = "x"
+                paperArray[rowIndex] = "".join(currentRow)
+
+for row in paperArray:
+    print(row)
+print(accessibleRolls)
+
+#accessiblePaperCheck(1, 0, 10, 10, paperArray)
